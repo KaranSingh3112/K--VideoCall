d7a273ad9f377a5b3660c27d73e02433b608433a
KaranSingh3112 <karansingh311206@gmail.com>
2026-06-10 21:29:40 +0530

    Deploying...

--- frontend/src/pages/VedioMeet.jsx
+++ frontend/src/pages/VedioMeet.jsx
@@ -14,9 +14,10 @@ import StopScreenShareIcon from "@mui/icons-material/StopScreenShare"
 import ChatIcon from "@mui/icons-material/Chat"
 import Badge from '@mui/material/Badge';
 import { useNavigate } from 'react-router';
+import server from '../environment';
 
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections = {};
 var pendingCandidates = {};
